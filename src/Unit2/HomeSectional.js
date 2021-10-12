9fdc184c8b17785c7f9b83167daadd4afffe48b4
Justin Peczenij <justinpeczenij@gmail.com>
2021-10-11 21:03:30 -0400

    remove extra html from home

--- src/Unit2/HomeSectional.js
+++ src/Unit2/HomeSectional.js
@@ -1,15 +1,9 @@
-import React from 'react';
-
+import React from "react";
 
 export default function Home(props) {
-   
   return (
-    <div className='home-sectional'>
-        <h2>
-            HOME SECTION Styling...
-        </h2>
-          <div className='home-images'>
-        </div>
+    <div className="home-sectional">
+      <div className="home-images"></div>
     </div>
   );
 }
