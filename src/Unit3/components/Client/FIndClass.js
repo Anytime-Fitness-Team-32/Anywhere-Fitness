0051a3d60572da0ebd40a759281b1bf349e4ea57
Justin Peczenij <justinpeczenij@gmail.com>
2022-01-16 11:34:29 -0500

    remove homesection styling

--- src/Unit3/components/Client/FIndClass.js
+++ src/Unit3/components/Client/FIndClass.js
@@ -1,72 +1,70 @@
-
-import { useState } from 'react'
+import { useState } from "react";
 
 const initialFormValues = {
-    starttime : "",
-    date : "",
-    duration : "",
-    type : "",
-    intensity :"",
-    location : "",
-}
+  starttime: "",
+  date: "",
+  duration: "",
+  type: "",
+  intensity: "",
+  location: "",
+};
 
+export default function FindClass(props) {
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const { classes, setClasses } = props;
 
-export default function FindClass(props){
-  const [ formValues, setFormValues ] = useState(initialFormValues);
-  const {classes, setClasses} = props
-  
-   const handleChanges = (evt) => {
-       setFormValues({
-           ...formValues, 
-           [evt.target.name] : evt.target.value
-       })
-      setClasses(classes.filter(c => c.duration === (formValues.duration)))
-   }
+  const handleChanges = (evt) => {
+    setFormValues({
+      ...formValues,
+      [evt.target.name]: evt.target.value,
+    });
+    setClasses(classes.filter((c) => c.duration === formValues.duration));
+  };
 
   return (
     <form>
-        <label htmlFor = "starttime"> Time: </label>
-        <input 
-        name = "starttime"
-        type = "text"
-        value = {formValues.time}
-        onChange = {handleChanges}
-        />
-        <label htmlFor = "date"> Date: </label>
-        <input 
-        name = "date"
-        type = "text"
-        value = {formValues.date}
-        onChange = {handleChanges}
-        />
-        <label htmlFor = "duration"> Duration: </label>
-        <input 
-        name = "duration"
-        type = "text"
-        value = {formValues.duration}
-        onChange = {handleChanges}
-        />
-        <label htmlFor = "type"> Class Type: </label>
-        <input 
-        name = "type"
-        type = "text"
-        value = {formValues.type}
-        onChange = {handleChanges}
-        />
-        <label htmlFor = "intensity"> Intensity: </label>
-        <input 
-        name = "intensity"
-        type = "text"
-        value = {formValues.intensity}
-        onChange = {handleChanges}
-        />
-        <label htmlFor = "location"> Location: </label>
-        <input 
-        name = "location"
-        type = "text"
-        value = {formValues.location}
-        onChange = {handleChanges}
-        />
+      <label htmlFor="starttime"> Time: </label>
+      <input
+        name="starttime"
+        type="text"
+        value={formValues.time}
+        onChange={handleChanges}
+      />
+      <label htmlFor="date"> Date: </label>
+      <input
+        name="date"
+        type="text"
+        value={formValues.date}
+        onChange={handleChanges}
+      />
+      <label htmlFor="duration"> Duration: </label>
+      <input
+        name="duration"
+        type="text"
+        value={formValues.duration}
+        onChange={handleChanges}
+      />
+      <label htmlFor="type"> Class Type: </label>
+      <input
+        name="type"
+        type="text"
+        value={formValues.type}
+        onChange={handleChanges}
+      />
+      <label htmlFor="intensity"> Intensity: </label>
+      <input
+        name="intensity"
+        type="text"
+        value={formValues.intensity}
+        onChange={handleChanges}
+      />
+      <label htmlFor="location"> Location: </label>
+      <input
+        name="location"
+        type="text"
+        value={formValues.location}
+        onChange={handleChanges}
+      />
     </form>
-  )
+  );
 }
